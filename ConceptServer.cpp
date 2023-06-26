14a71ddab71e5128a35e93a7fa3c95b02b3400a8
SahilShaik1 <89698739+SahilShaik1@users.noreply.github.com>
2023-06-26 10:08:51 -0400

    Create ConceptServer.cpp

---
+++ ConceptServer.cpp
@@ -0,0 +1,152 @@
+//Concept of outsorcing implemented
+
+#include <windows.h>
+#include <iostream>
+#include <cstddef>
+#include <vector>
+
+#define WSA_SUC 0
+#define BIND_SUC 0
+#define LIST_SUC 0
+#define MAX_CONNECTIONS 1
+
+struct problem{
+    //Format of problem
+    std::vector<std::vector<double>> mat;
+    double scalar;
+    std::string command;
+
+    problem(std::vector<std::vector<double>> mat, double scalar, std::string command){
+        this->mat = mat;
+        this->scalar = scalar;
+        this->command = command;
+    }
+};
+
+static void reportErr(SOCKET* s = NULL, std::vector<SOCKET*> workers = {NULL}){
+    if(s != NULL){
+        closesocket(*s);
+    }
+    if(workers[0] != NULL){
+        for(SOCKET* worker : workers){
+            closesocket(*worker);
+        }
+    }
+    std::cout << "Error: " << WSAGetLastError() << "\n";
+    WSACleanup();
+    abort();
+}
+
+
+
+struct data {
+    int f;
+    data(int f = 0){
+        this->f = f;
+    }; 
+};
+
+
+
+
+
+int main() {
+    WORD verReq = MAKEWORD(2, 2);
+    std::string ip = "127.0.0.1";
+    unsigned short const port = 55555;
+    WSAData wsaDataHolder;
+    
+
+    int wsa = WSAStartup(verReq, &wsaDataHolder);
+    if (wsa == WSA_SUC){
+        std::cout << "Startup Successful.\n";
+        std::cout << "Version Requested: " << wsaDataHolder.wVersion << ".\n";
+        std::cout << "Description: " << wsaDataHolder.szDescription << ".\n";
+    } else {
+        std::cout << "Failed Startup.\n";
+        std::cout << "Error Code: " << wsa << "\n";
+        WSACleanup();
+        abort();
+    }
+
+
+
+    SOCKET connection = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    
+    if(connection  != INVALID_SOCKET){
+        std::cout << "Socket Creation Successful.\n";
+    } else {
+        std::cout << "Failed to create Socket.\n";
+        reportErr(&connection);
+    }
+    sockaddr_in connectionInfo;
+
+
+    //htons and inet simply convert strings/shorts to the correct format
+    connectionInfo.sin_family = AF_INET;
+    connectionInfo.sin_addr.S_un.S_addr = INADDR_ANY;
+    PVOID refAddr = &connectionInfo.sin_addr.s_addr;
+    connectionInfo.sin_port = htons(port);
+
+    int bindRes = bind(connection, (SOCKADDR*)&connectionInfo, sizeof(connectionInfo));
+
+
+    if (bindRes == BIND_SUC){
+        std::cout << "Successfully binded socket.\n";
+    } else {
+        std::cout << "Failed to bind socket.\n";
+        reportErr(&connection);
+    }
+
+    
+
+
+    int listRes = listen(connection, MAX_CONNECTIONS);
+    if(listRes != INVALID_SOCKET){
+        std::cout << "Successfully started Listening...\n";
+    } else {
+        std::cout << "Failed to listen.\n";
+        reportErr(&connection);
+    }
+
+
+    std::vector<SOCKET*> workers;
+    
+    while(workers.size() < MAX_CONNECTIONS){
+        SOCKET worker;
+        sockaddr_in workerInfo;
+        worker = accept(connection, (SOCKADDR* )&workerInfo, NULL);
+        if (worker != INVALID_SOCKET){
+            std::cout << "Connected with a Client on port "<< workerInfo.sin_port << "\n";
+            workers.push_back(&worker);
+        } else {
+            std::cout << "Failed to accept.\n";
+            reportErr(&connection, workers);
+        }
+    }
+
+    data dat(3);
+    send(*workers[0], (char *)&dat, sizeof(data), 0);
+    /*
+    std::vector<std::vector<double>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    for(int i = 0; i < workers.size(); i++){
+        const char* cmd = "SCALAR";
+        problem* prob = new problem(matrix, 3, cmd);
+        std::cout << "Command Ready: " << prob->command << "\n";
+        
+        char* addr = (char*)prob;
+        
+        int res = send(*(workers[i]), addr, sizeof(addr), 0);
+        if(res != sizeof(cmd)){
+            std::cout << "Error in sending Command.\n";
+            reportErr(&connection, workers);
+        } else {
+            std::cout << "Sent Command.\n";
+        }
+    }
+    */
+
+    
+
+    return 0;
+}
